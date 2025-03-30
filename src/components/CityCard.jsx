3bce991ca6085df40c4a5bbecb17c12e701e9bbc
Prince Hlela <mphdigital@protonmail.com>
2025-03-30 11:15:54 +0200

    created a search functionality using zustand to store the state and fetch data from the api

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import sun from '../assets/sun.svg'
+import useWeatherStore from '../stores/WeatherStore'
 
 function CityCard() {
+
+    // retreiving the weatherData array from the zustand store on the WeatherStore.js file
+    const data = useWeatherStore((state) => state.weatherData)
+
     return (
         <div className='bg-white rounded-xl px-[30px] py-[40px] text-[var(--main-color)] flex gap-6 flex-col text-center justify-center items-center shadow rounded-xl h-[100%]'>
-            <h1 className='text-[30px] font-semibold'>New York</h1>
+            <h1 className='text-[30px] font-semibold'>{data.name}</h1>
             <img src={sun} alt="sun picture" className='w-[100px]' />
         </div>
     )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import useWeatherStore from '../stores/WeatherStore';
 
 function Search() {
+
+    // creating a reference
+    const inputRef = useRef("");
+
+    // grabbing the search functionality from the zustand store
+    const search = useWeatherStore((state) => state.search);
+
+    // grab the data from input and search the keyword or city and clear the input
+    function searchCity() {
+        const city = inputRef.current.value;
+        console.log(city)
+        search(city);
+        inputRef.current.value = "";
+    }
+
+    // function to stop the default behavior of the browser on form subnit
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <form className='bg-white p-[5px] rounded-xl max-w-[500px] w-[100%] flex gap-5'>
-            <input type="text" className='flex-grow p-3 text-[var(--main-color)] border-none outline-nones' placeholder='Enter your city' />
-            <button className='bg-[var(--accent-color)] text-[var(--main-color)] font-medium px-[20px] py-[10px] rounded-xl'>Search</button>
+        <form className='bg-white p-[5px] rounded-xl max-w-[500px] w-[100%] flex gap-5' onSubmit={handleSubmit}>
+            <input type="text" className='flex-grow p-3 text-[var(--main-color)] border-none outline-nones' placeholder='Enter your city' ref={inputRef} />
+            <button className='bg-[var(--accent-color)] text-[var(--main-color)] font-medium px-[20px] py-[10px] rounded-xl' onClick={searchCity}>Search</button>
         </form>
     )
 }
